0b4c39c381ab831660cd6ebfb33340735bece1c1
Khaled Afify <ikhaledsayed@gmail.com>
2020-06-20 20:25:02 +0200

    update user model and user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username','email','is_host')
+    list_filter = ('is_host','gender')
+    fieldsets = UserAdmin.fieldsets + ( ('Customer Profile',{
+        'fields': ('gender','avatar','is_host','country','currency')
+    }),)
+
+    print(fieldsets)
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    COUNTRY_EGYPT = "EG"
+    COUNTRY_UK = "UK"
+    country_choices = (
+        (COUNTRY_EGYPT,"Egypt"),
+    )
+    DEFAULT_COUNTRY = COUNTRY_EGYPT
+
+    GENDER_MALE = "MALE"
+    GENDER_FEMALE = "FEMALE"
+    GENDER_OTHER = "OTHER"
+    GENDER_CHOICES = (
+        (GENDER_MALE,"Male"),
+        (GENDER_FEMALE,"Female"),
+        (GENDER_OTHER,"Other"),
+    )
+
+    CURRENCY_EGP = "EGP"
+    CURRENCY_CHOICES = (
+        (CURRENCY_EGP,"Egyptian Pound"),
+    )
+    DEFAULT_CURRENCY = CURRENCY_EGP
+
+    avatar = models.ImageField(null=True,blank=True)
+    birthdate = models.DateField(null=True,blank=True)
+    country = models.CharField(
+                               choices=country_choices,
+                               default=DEFAULT_COUNTRY,
+                               null=False,
+                               blank=False,
+                               max_length=20)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES,
+        default=DEFAULT_CURRENCY,
+        null=False,
+        blank = False,
+        max_length=3
+    )
+    gender = models.CharField(
+        choices=GENDER_CHOICES ,
+        null=True,
+        blank=True,
+        max_length=7
+    )
+
+    is_host = models.BooleanField(default=False)
